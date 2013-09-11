ed678be1f0b45b80bfc2b219b658fbe9c20467e6
Kevin Sun <sunkev@brandeis.edu>
2013-09-11 15:25:32 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musicnave::Application.config.secret_key_base = '245efd96609425f8f2dcaaadaac6e54ed36a555f39263751626a4eb54ae0d5e6445f4d5952c62c3f3b91346dc0bd7dead1265f55f254df4aaf86de77db0dba96'
